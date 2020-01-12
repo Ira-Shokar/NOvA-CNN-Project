dd4f1ed18fa903e8c80accb4b025012a3a54fbb9
Ira Shokar <45093826+Ira-Shokar@users.noreply.github.com>
2020-01-12 10:52:10 +0000

    Create train.py

---
+++ train.py
@@ -0,0 +1,8 @@
+from functions import *
+from methods import *
+
+train(path = '/home/ishokar/december_test/output/',
+      epochs= 200,
+      batch_size = 32,
+      call_back_patience = 20,
+      name = '32_Adam')
