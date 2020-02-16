88cc10448ae5953bf21a5af548f2a699e5b2719e
Ira Shokar <45093826+Ira-Shokar@users.noreply.github.com>
2020-02-16 19:27:24 +0100

    Update train.py

--- train.py
+++ train.py
@@ -1,8 +1,12 @@
-from functions import *
 from methods import *
 
-train(path = '/home/ishokar/december_test/output/',
-      epochs= 200,
+# model_optimiser options: 'Adam', 'SGD'
+train(path = '/home/ishokar/dataframes/',
+      train_type = 'descr',
+      epochs = 100,
       batch_size = 32,
+      dataset_percent = 0.01,
       call_back_patience = 20,
-      name = '32_Adam')
+      learning_rate = 0.0001,
+      model_optimiser='SGD',
+      out_file_name = 'descr_100__32_SGD_12_02')
