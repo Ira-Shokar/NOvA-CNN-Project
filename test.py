20746520aacc71f0562df26287fb8fa79de4727e
Ira Shokar <45093826+Ira-Shokar@users.noreply.github.com>
2020-02-16 19:26:52 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,47 +1,14 @@
-from functions import *
 from methods import *
 
-def test_generator(batch_size, dataset):
-    batch_images = np.zeros((batch_size, 2, 80, 100))
-    size = dataset.shape[0]
-    while True:
-        for i in range(batch_size):
-            index= random.randint(0,size-1)
-            while index in index_list:
-                index= random.randint(1,size)-1
-            index_list.append(index)
-            row = dataset.loc[index]
-            file= row['file']
-            images  = image(maps(file)[row['train_index']])
-            images= (images - np.min(images))/ (np.max(images) - np.min(images))
-            batch_images[i] = images
+#function paramemters
 
-        yield batch_images
+path = '/home/ishokar/dataframes/'
 
-def test(weights_file, path, batch_no):
-    df8 = open_df(path, 8)
-    df9 = open_df(path, 9)
-    test = [df8, df9]
+data = 'both'
+model_type = 'default'
+output = 'default'
 
-    df_test = pd.concat(test)
-    df_test.index = range(len(df_test['file']))
-    steps_per_epoch = round(len(df_test['file'])/(batch_no+1))
+file= 'weights_genie_200__32_SGD_12_02.h5'
+file_name = 'default_TOboth'
 
-    model = mobilenetv2.MobileNetV2(input_shape=((2, 80, 100),), classes=3)
-    model.load_weights('/home/ishokar/december_test/' + weights_file)
-
-    probabilities = model.predict_generator(test_generator(batch_no, df_test), steps = steps_per_epoch)
-    test_labels_list = test_labels(df_test, index_list)
-
-    with open('probabilities_{}.pkl'.format(i[:-3]),'wb') as f1:
-                    pkl.dump(probabilities, f1)
-
-    with open('test_labels_list_{}.pkl'.format(i[:-3]),'wb') as f1:
-                    pkl.dump(test_labels_list, f1)
-
-
-path = '/home/ishokar/december_test/output/'
-batch_no = 32
-index_list = []
-for i in ['weights_32_2_LR_0.0001.h5']:
-    test(i, path, batch_no)
+test(file, path, file_name, data, model_type, output)
