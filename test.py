55f2c3665df7af50398073f55268b57830c48e79
Ira Shokar <45093826+Ira-Shokar@users.noreply.github.com>
2020-03-02 17:12:01 +0000

    Update test.py

--- test.py
+++ test.py
@@ -5,10 +5,12 @@ from methods import *
 path = '/home/ishokar/dataframes/'
 
 data = 'both'
-model_type = 'default'
+model_type = 'descr'
 output = 'default'
 
-file= 'weights_genie_200__32_SGD_12_02.h5'
-file_name = 'default_TOboth'
+file= '/weights_train_100_descr_32_sgd_22_02.h5'
+file_name = 'default_TOboth_equal_1'
 
-test(file, path, file_name, data, model_type, output)
+dataset_percent = 0.001
+
+test(file, path, file_name, dataset_percent, data, model_type, output)
