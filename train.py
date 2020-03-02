3c5c88cef0d01cb3b8e2e58bb05028996788b2d6
Ira Shokar <45093826+Ira-Shokar@users.noreply.github.com>
2020-03-02 17:10:35 +0000

    Update train.py

--- train.py
+++ train.py
@@ -1,12 +1,13 @@
 from methods import *
 
 # model_optimiser options: 'Adam', 'SGD'
-train(path = '/home/ishokar/dataframes/',
-      train_type = 'descr',
-      epochs = 100,
+
+train(train_type = 'dann',
+      epochs= 100,
       batch_size = 32,
-      dataset_percent = 0.01,
-      call_back_patience = 20,
-      learning_rate = 0.0001,
-      model_optimiser='SGD',
-      out_file_name = 'descr_100__32_SGD_12_02')
+      dataset_percent = 0.9,
+      call_back_patience = 10,
+      learning_rate = 0.001,
+      DANN_strength = 0.1,
+      model_optimiser='Adam',
+      out_file_name = '32_Adam_dann_0.5_02_03')
