d6a241bebe69ed6f69d3bf72c65eb6bc20dedd00
Ira Shokar <45093826+Ira-Shokar@users.noreply.github.com>
2020-03-05 00:30:40 +0000

    Update cuts.py

--- cuts.py
+++ cuts.py
@@ -1,4 +1,5 @@
 from functions import *
 from methods import *
 
+#function that applies the cuts method to a folder containing hdf5 files and returns a dataframe
 cuts('/unix/nova/hdf5/ND-ProngCVN-FHC')
