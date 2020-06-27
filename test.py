617645dde79281a0d1c85f94861649a39ba5c7b5
Ira Shokar <45093826+Ira-Shokar@users.noreply.github.com>
2020-06-27 14:15:37 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,14 @@
-from methods import *
+from scripts.methods import *
 
 #function paramemters
 
 path = '/home/ishokar/dataframes/'
 
+#data options: 'both', 'genie', 'gibuu'
 data = 'both'
+#model_type options: 'default', 'descr'
 model_type = 'descr'
+#output options: 'default'
 output = 'default'
 
 file= '/weights_train_100_descr_32_sgd_22_02.h5'
