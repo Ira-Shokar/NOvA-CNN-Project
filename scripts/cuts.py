3e4860ab609464c91909798cbece9e88072b337d
Ira Shokar <45093826+Ira-Shokar@users.noreply.github.com>
2020-08-02 16:06:22 +0100

    Delete cuts.py

--- scripts/cuts.py
+++
@@ -1,5 +0,0 @@
-from scripts.functions import *
-from scripts.methods import *
-
-#function that applies the cuts method to a folder containing hdf5 files and returns a dataframe
-cuts('/unix/nova/hdf5/ND-ProngCVN-FHC')
